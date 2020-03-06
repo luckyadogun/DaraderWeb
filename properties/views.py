dc30bc1fb4ed9f358fb3d1db6c3fc8202f90266f
Lucky Adogun <meetluckyadogun@gmail.com>
2020-03-06 18:17:21 +0100

    updated property views with HomepageView

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "properties/home.html"
+
+    # def get(self, *args, **kwargs):
+    #     return HttpResponse("Hello World!")
