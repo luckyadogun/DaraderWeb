cf3c1ad30ae8523575423e21839906116070417f
Lucky Adogun <meetluckyadogun@gmail.com>
2020-03-10 00:39:26 +0100

    updated admin for properties and user models

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
-from django.contrib import admin 
+from django.contrib import admin
 from django.contrib.auth.models import Group
 
 from .models import User
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
