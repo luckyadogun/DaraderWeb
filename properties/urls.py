4c689d86e54ad2d906875b874a647c25fc8641aa
Lucky Adogun <meetluckyadogun@gmail.com>
2020-03-16 21:21:01 +0100

    search filter added

--- properties/urls.py
+++ properties/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import HomePageView
+from .views import (HomePageView, SearchResultView)
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('search/', SearchResultView.as_view(), name="search"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,12 @@
+from itertools import chain
+
+from django.urls import reverse
+from django.core import serializers
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.http import HttpResponseRedirect
 
-from .models import Property
-
+from .models import Property, PropertyDetails
 from .utils import get_currently_featured
 
 
@@ -12,10 +15,6 @@ class HomePageView(TemplateView):
     template_name = "properties/home.html"
 
     def get_context_data(self, **kwargs):
-        # TODO
-        # - add dummy properties
-        # - add properties to context
-        # - edit html code with for-loop
         context = super().get_context_data(**kwargs)
         context['for_sale'] = Property.forsale.order_by('created')[:3]
         context['for_rent'] = Property.forrent.order_by('created')[:3]
@@ -35,9 +34,38 @@ class HomePageView(TemplateView):
 
         search_title = self.request.POST.get("location", "")
         property_type = self.request.POST.get("property-type", "")
-        property_status = self.request.POST.get("property-status", "")
-        bedroom_size = self.request.POST.get("bedrooms", "")
-        bathrooms = self.request.POST.get("bathrooms", "")
-        budget = self.request.POST.get("budget", "")
+        property_category = self.request.POST.get("property-category", "")
+        bedrooms = self.request.POST.get("bedrooms", 1)
+        bathrooms = self.request.POST.get("bathrooms", 1)
+        budget = self.request.POST.get("budget").split('-')
 
-        return render(request, self.template_name, {})
+        q1 = PropertyDetails.objects.filter(
+            property_obj__title__icontains=search_title)
+        q2 = PropertyDetails.objects.filter(
+            property_obj__property_type=property_type)
+        q3 = PropertyDetails.objects.filter(
+            property_obj__property_category=property_category)
+        q4 = PropertyDetails.objects.filter(bedrooms=int(bedrooms))
+        q5 = PropertyDetails.objects.filter(bathrooms=int(bathrooms))
+        q6 = PropertyDetails.objects.filter(
+            property_obj__price__range=(
+                int(budget[0]), int(budget[1])))
+
+        query = list(set(chain(q1, q2, q3, q4, q5)))
+        data = serializers.serialize("json", query)
+        self.request.session['query'] = data
+
+        return HttpResponseRedirect('/search/')
+
+
+class SearchResultView(TemplateView):
+
+    template_name = "properties/serp.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if 'query' in self.request.session:
+            print(self.request.session['query'])
+
+        return context
