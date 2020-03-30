67159504637f03321cbbd15ae0214f95bfe83824
Lucky Adogun <meetluckyadogun@gmail.com>
2020-03-30 17:39:09 +0100

    dashboard added

--- project/urls.py
+++ project/urls.py
@@ -1,18 +1,3 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,9 +7,12 @@ admin.site.site_header = "Darader Admin Portal"
 admin.site.site_title = "Darader Admin Portal"
 admin.site.index_title = "Welcome to Darader Admin Portal"
 
+handler404 = "properties.views.handler404"
+
 urlpatterns = [
     path('secret-saucer/', admin.site.urls),
-    path('', include('properties.urls'), name='properties'),
+    path('', include(('properties.urls', 'properties'), namespace='properties')),
+    path('my/', include(('users.urls', 'users'), namespace='users')),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import View
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+
+class LoginView(View):
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(
+            username=username,
+            password=password
+            )
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(
+                    reverse("dashboard")
+                )
+
+
+class DashboardView(TemplateView):
+    template_name = "users/index.html"
+
+
+class MyPropertyView(TemplateView):
+    template_name = "users/my_properties.html"
+
+
+class MyBookingsView(TemplateView):
+    template_name = "users/my_bookings.html"
+
+
+class AddPropertyView(TemplateView):
+    template_name = "users/add_property.html"
+
+
+class MyAccountView(TemplateView):
+    template_name = "users/my_account.html"
+
+
+class UpdateAccountView(TemplateView):
+    template_name = "users/update_account.html"
+
+
+class BookmarkedView(TemplateView):
+    template_name = "users/bookmarks.html"
