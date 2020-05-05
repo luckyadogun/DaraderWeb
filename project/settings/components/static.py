4a4f3ace9536bacbe23988da6642e3f1e622d423
Lucky Adogun <meetluckyadogun@gmail.com>
2020-05-05 09:09:35 +0100

    updated lockfile

--- project/settings/components/static.py
+++ project/settings/components/static.py
@@ -26,4 +26,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-    # DEFAULT_FILE_STORAGE = 'storage.MediaStorage'
+    # DEFAULT_FILE_STORAGE = 'storage_backend.MediaStorage'
