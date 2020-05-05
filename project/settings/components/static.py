26372131304c0b50ed8fd17ee15771f74f9ba91a
Lucky Adogun <meetluckyadogun@gmail.com>
2020-05-05 09:28:57 +0100

    set S3

--- project/settings/components/static.py
+++ project/settings/components/static.py
@@ -4,26 +4,27 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+if os.environ.get("ENVIRONMENT") != "local":
+    AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
+    AWS_SECRET_ACCESS_KEY = os.environ.get(AWS_ACCESS_KEY_ID)
+    AWS_STORAGE_BUCKET_NAME = 'darader-assets'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amamzonaws' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
 
-# if os.environ.get("ENVIRONMENT") != "local":
-    # AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
-    # AWS_SECRET_ACCESS_KEY = os.environ.get(AWS_ACCESS_KEY_ID)
-    # AWS_STORAGE_BUCKET_NAME = 'darader-assets'
-    # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amamzonaws' % AWS_STORAGE_BUCKET_NAME
+    AWS_LOCATION = 'static'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-    # AWS_S3_OBJECT_PARAMETERS = {
-    #     'CacheControl': 'max-age=86400',
-    # }
+    DEFAULT_FILE_STORAGE = 'storage_backend.MediaStorage'
+else:
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-    # AWS_LOCATION = 'static'
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
-    # DEFAULT_FILE_STORAGE = 'storage_backend.MediaStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
