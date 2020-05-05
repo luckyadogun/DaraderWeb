f60dac837fdf9ff37a7f47af887c7c214c39c4a9
Lucky Adogun <meetluckyadogun@gmail.com>
2020-05-05 13:02:04 +0100

    update static settings

--- project/settings/components/static.py
+++ project/settings/components/static.py
@@ -14,12 +14,12 @@ if os.getenv("ENVIRONMENT") != "local":
     # AWS static settings
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.StaticStorage'
+    STATICFILES_STORAGE = 'storage_backend.StaticStorage'
 
     # AWS media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'storage_backend.PublicMediaStorage'
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
