1cba3d25e28de819657300b8789257eeb192c068
Lucky Adogun <meetluckyadogun@gmail.com>
2020-05-07 16:01:48 +0100

    settings added for prod

---
+++ project/settings/environments/prod.py
@@ -0,0 +1,11 @@
+from staging import *
+
+
+ENVIRONMENT = "live"
+DEBUG = False
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "infinite-mountain-88972.herokuapp.com",
+    "darader.com",
+    ]
