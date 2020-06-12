3c9e2df839ef66da162ff09a786a55dcbc37bf40
Lucky Adogun <meetluckyadogun@gmail.com>
2020-06-12 15:30:51 +0100

    settings rolledback for heroku

--- project/settings/environments/prod.py
+++ project/settings/environments/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -11,26 +12,16 @@ ALLOWED_HOSTS = [
     "darader.com",
     ]
 
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "PORT": 5432,
-        "NAME": DB_NAME,
-        "HOST": "localhost",
-        "ATOMIC_REQUESTS": True,
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
-SESSION_COOKIE_SECURE=True
-CSRF_COOKIE_SECURE=True
-SECURE_HSTS_SECONDS=31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS=True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
