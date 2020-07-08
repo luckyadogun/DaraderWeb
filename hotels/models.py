26229ba7c1d470be35852d3a251fecd3aa8533cf
Adedeji Saheed Ademola <sademolaadedeji@gmail.com>
2020-07-08 10:55:00 +0100

    Updated the hotels model

--- hotels/models.py
+++ hotels/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from properties.models import TimeStampedModel
+from users.models import User
 
 # Create your models here.
-class Hotel(models.Model):
+class Hotel(TimeStampedModel):
     HOTEL_TYPE_CHOICES = (
         ("Hotel", "Hotel"),
     )
-    name = models.CharField(_("company name"), max_length=200, unique=True)
+    name = models.CharField(_("name"), max_length=200, unique=True)
     average_price = models.DecimalField(max_digits=10, decimal_places=2)
     address = models.TextField(_("address"))
-    hotel_type = models.CharField(_("hotel type"), choices=HOTEL_TYPE_CHOICES, blank=True)
+    hotel_type = models.CharField(_("hotel type"), max_length=200, choices=HOTEL_TYPE_CHOICES, blank=True)
     number_of_rooms = models.IntegerField(_("number of rooms"))
     description = models.TextField(_("description"))
     has_restaurant = models.BooleanField(_("restaurant"), default=False)
@@ -17,7 +19,7 @@ class Hotel(models.Model):
     has_wireless_internet = models.BooleanField(_("wireless internet"), default=False)
     has_24_hrs_electricity = models.BooleanField(_("24 hrs electricity"), default=False)
     has_adequate_parking_space = models.BooleanField(_("adequate parking space"), default=False)
-    has_swimming_pool  models.BooleanField(_("swimming pool"), default=False)
+    has_swimming_pool = models.BooleanField(_("swimming pool"), default=False)
     has_car_rental = models.BooleanField(_("car rental"), default=False)
     has_double_bed = models.BooleanField(_("double bed"), default=False)
     has_toiletries = models.BooleanField(_("toiletries"), default=False)
@@ -32,8 +34,9 @@ class Hotel(models.Model):
     has_spa_treatment = models.BooleanField(_("spa treatment"), default=False)
     has_night_club = models.BooleanField(_("night club"), default=False)
     has_luggage_storage = models.BooleanField(_("luggage storage"), default=False)
-    has_air_condititoning = models.BooleanField(_("air conditioning"), default=False)
+    has_air_conditioning = models.BooleanField(_("air conditioning"), default=False)
     has_car_wash = models.BooleanField(_("car wash"), default=False)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -57,7 +60,7 @@ class Room(models.Model):
 class FAQ(models.Model):
     question = models.CharField(_("question"), max_length=200)
     answer = models.CharField(_("answer"), max_length=200)
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, null=True)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question
