10a979fc10cc3d252f6711852961f70df1c2d81d
Lucky Adogun <meetluckyadogun@gmail.com>
2020-07-10 17:56:08 +0100

    updated add hotel

--- hotels/forms.py
+++ hotels/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Hotel, HotelPhotos, Room, FAQ
 
+
 class HotelPhotosForm(forms.ModelForm):
     photo = forms.ImageField(required=False, widget=forms.ClearableFileInput(
         attrs={
@@ -10,6 +11,7 @@ class HotelPhotosForm(forms.ModelForm):
     class Meta:
         model = HotelPhotos
         exclude = ('hotel',)
+
 
 class RoomForm(forms.ModelForm):
     class Meta:
@@ -26,27 +28,28 @@ class RoomForm(forms.ModelForm):
             'information': forms.Textarea(attrs={
                 'class': 'form-control',
                 'placeholder': 'Enter the room description',
+                'rows': 3
             })
         }
 
+
 class FAQForm(forms.ModelForm):
-    
     class Meta:
         model = FAQ
         exclude = ('hotel',)
         widgets = {
-            'question':forms.TextInput(attrs={
+            'question': forms.TextInput(attrs={
                 'class': 'form-control filter-input',
                 'placeholder': 'Enter the question',
                 'required': 'true'}),
-            'answer':forms.TextInput(attrs={
+            'answer': forms.TextInput(attrs={
                 'class': 'form-control filter-input',
                 'placeholder': 'Enter the answer',
                 'required': 'true'}),
         }
 
+
 class HotelForm(forms.ModelForm):
-    
     class Meta:
         model = Hotel
         exclude = ('creator',)
@@ -72,9 +75,9 @@ class HotelForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Describe the hotel',
             }),
-            'has_restaurant':forms.CheckboxInput(attrs={
+            'has_restaurant': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-a'}),
-            'has_bar' :forms.CheckboxInput(attrs={
+            'has_bar': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-b'}),
             'has_wireless_internet': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-c'}),
@@ -82,38 +85,38 @@ class HotelForm(forms.ModelForm):
                 'class': 'form-control filter-input', 'id': 'check-d'}),
             'has_adequate_parking_space': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-e'}),
-            'has_swimming_pool' : forms.CheckboxInput(attrs={
+            'has_swimming_pool': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-f'}),
-            'has_car_rental' : forms.CheckboxInput(attrs={
+            'has_car_rental': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-g'}),
-            'has_double_bed' : forms.CheckboxInput(attrs={
+            'has_double_bed': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-h'}),
-            'has_toiletries' : forms.CheckboxInput(attrs={
+            'has_toiletries': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-i'}),
-            'has_concierge' : forms.CheckboxInput(attrs={
+            'has_concierge': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-j'}),
-            'has_shower' : forms.CheckboxInput(attrs={
+            'has_shower': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-k'}),
-            'has_room_service' : forms.CheckboxInput(attrs={
+            'has_room_service': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-l'}),
-            'has_key_card_system' : forms.CheckboxInput(attrs={
+            'has_key_card_system': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-m'}),
-            'has_gym' : forms.CheckboxInput(attrs={
+            'has_gym': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-n'}),
-            'has_airport_pickup' : forms.CheckboxInput(attrs={
+            'has_airport_pickup': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-o'}),
-            'has_car_hire' : forms.CheckboxInput(attrs={
+            'has_car_hire': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-p'}),
-            'has_laundry' : forms.CheckboxInput(attrs={
+            'has_laundry': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-q'}),
-            'has_spa_treatment' : forms.CheckboxInput(attrs={
+            'has_spa_treatment': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-r'}),
-            'has_night_club' : forms.CheckboxInput(attrs={
+            'has_night_club': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-s'}),
-            'has_luggage_storage' : forms.CheckboxInput(attrs={
+            'has_luggage_storage': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-t'}),
-            'has_air_conditioning' : forms.CheckboxInput(attrs={
+            'has_air_conditioning': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-u'}),
-            'has_car_wash'  : forms.CheckboxInput(attrs={
+            'has_car_wash': forms.CheckboxInput(attrs={
                 'class': 'form-control filter-input', 'id': 'check-v'})            
         }
