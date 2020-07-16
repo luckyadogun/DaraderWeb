aa92b1eb8793e1380459fad742916b523e19b10d
Adedeji Saheed Ademola <sademolaadedeji@gmail.com>
2020-07-16 16:05:17 +0100

    Added the hotel list page with authentication

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
 from .views import (add_hotel_view, hotel_details, hotel_booking_view, 
-                    MyHotelView, update_hotel, hotel_delete_view
+                    MyHotelView, update_hotel, hotel_delete_view, AllHotelView
                     )
 
 urlpatterns = [
     path('add-hotel/', add_hotel_view, name="add-hotel"),
-    path('<int:pk>/', hotel_details, name="hotel-details"),
+    path('<str:name>', hotel_details, name="hotel-details"),
     path('room-booking/', hotel_booking_view, name="hotel-room-booking"),
-    path('<int:pk>/update/', update_hotel, name="update-hotel"),
+    path('<str:name>/update/', update_hotel, name="update-hotel"),
     path('ajax/hotel/delete/', hotel_delete_view, name="delete-hotel"),
-    path('my-hotels/', MyHotelView.as_view(), name="my-hotels")
+    path('my-hotels/', MyHotelView.as_view(), name="my-hotels"),
+    path('all-hotels/', AllHotelView.as_view(), name="all-hotels")
     # path('hotel-bookings/', MyHotelBookingsView.as_view(), name="my-hotel-bookings"),
     # path('ajax/bookings/setup/', booking_setup_view, name="setup-bookings"),
     # path('ajax/bookings/delete/', booking_delete_view, name="delete-bookings"),
--- hotels/views.py
+++ hotels/views.py
@@ -72,11 +72,11 @@ def add_hotel_view(request):
     return render(request, 'hotels/add_hotel.html', context)
 
 
-@login_required
-def hotel_details(request, pk):
+# @login_required
+def hotel_details(request, name):
     context = {}
 
-    obj = Hotel.objects.get(id=pk)
+    obj = Hotel.objects.get(name=name)
 
     if request.method == "POST":
         hotel_bookingform = HotelBookingRequestForm(request.POST)
@@ -113,7 +113,7 @@ class MyHotelView(LoginRequiredMixin, ListView):
         if self.request.user.is_staff:
             return Hotel.objects.filter(creator=self.request.user)
 
-@login_required
+@login_required(login_url='properties:login')
 def hotel_booking_view(request):
     room_id = request.POST.get('room_id')
     hotel_id = request.POST.get('hotel_id')
@@ -148,10 +148,10 @@ def hotel_delete_view(request):
 
 @login_required
 @staff_or_manager_required
-def update_hotel(request, pk=None):
+def update_hotel(request, name=None):
     context = {}
 
-    hotel_obj = get_object_or_404(Hotel, id=pk)
+    hotel_obj = get_object_or_404(Hotel, name=name)
 
     if not hotel_obj.creator == request.user:
         raise Http404("Unauthorized access!")
@@ -209,3 +209,101 @@ def update_hotel(request, pk=None):
     context["hotel"] = hotel_obj
 
     return render(request, 'hotels/update_hotel.html', context)
+
+# ALL HOTELS
+class AllHotelView(ListView):
+    template_name = "hotels/all_hotels.html"
+    context_object_name = "hotels"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Hotel.objects.order_by("created").reverse()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recent_hotels"] = Hotel.objects.order_by("created").reverse()[:3]
+        return context
+
+# class SearchResultView(ListView):
+
+#     template_name = "properties/serp.html"
+#     context_object_name = "properties"
+#     paginate_by = 5
+
+#     def get_queryset(self):
+
+#         search_title = self.request.GET.get("location", "")
+#         property_type = self.request.GET.get("property-type", "")
+#         property_category = self.request.GET.get("property-category", "")
+#         bedrooms = self.request.GET.get("bedrooms", 1)
+#         bathrooms = self.request.GET.get("bathrooms", 1)
+#         budget = self.request.GET.get("budget", "200000 - 300000").split("-")
+#         basketball = self.request.GET.get("basketball")
+#         swimming_pool = self.request.GET.get("swimming-pool")
+#         gym = self.request.GET.get("gym")
+#         wheelchair = self.request.GET.get("wheelchair")
+
+#         submit_action = self.request.GET.get("submit")
+
+#         new_query = PropertyDetails.objects.filter(
+#             Q(property_obj__title__icontains=search_title) |
+#             Q(property_obj__property_type=property_type) |
+#             Q(property_obj__property_category=property_category) |
+#             Q(bedrooms=int(bedrooms)) | Q(bathrooms=int(bathrooms)) |
+#             Q(property_obj__price__range=(int(budget[0]), int(budget[1]))) |
+#             Q(has_basketball_court=bool(basketball)) |
+#             Q(has_swimming_pool=bool(swimming_pool)) | Q(has_gym=bool(gym)) |
+#             Q(is_wheelchair_friendly=bool(wheelchair))
+#             )
+
+#         category_param = ["rent", "sale", "lease", ]
+
+#         type_param = [
+#             "flat", "land",
+#             "house", "commercial",
+#             "event centre", ]
+
+#         if self.kwargs["queryset"] == "search":
+#             if submit_action:
+#                 return new_query
+#             else:
+#                 try:
+#                     return [PropertyDetails.objects.get(pk=qs) for qs in self.request.session["query"]]
+#                 except KeyError:
+#                     return ""
+
+#         if self.kwargs["queryset"] in category_param:
+#             if submit_action:
+#                 return new_query
+#             else:
+#                 return PropertyDetails.objects.filter(
+#                     property_obj__property_category=self.kwargs["queryset"])
+
+#         if self.kwargs["queryset"] in type_param:
+#             if submit_action:
+#                 return new_query
+#             else:
+#                 return PropertyDetails.objects.filter(
+#                     property_obj__property_type=self.kwargs["queryset"])
+
+#         if self.kwargs["queryset"] == "all":
+#             if submit_action:
+#                 return new_query
+#             else:
+#                 return PropertyDetails.objects.all()
+
+#     def get_context_data(self, *args, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["qs"] = self.request.META["QUERY_STRING"]
+#         context["recent_properties"] = Property.objects.order_by("created")[:5]
+#         context["total_flats"] = Property.objects.filter(
+#                                     property_type="flat").count()
+#         context["total_houses"] = Property.objects.filter(
+#                                     property_type="house").count()
+#         context["total_lands"] = Property.objects.filter(
+#                                     property_type="land").count()
+#         context["total_commercials"] = Property.objects.filter(
+#                                     property_type="commercial").count()
+#         context["total_event_centres"] = Property.objects.filter(
+#                                     property_type="event centre").count()
+#         return context
